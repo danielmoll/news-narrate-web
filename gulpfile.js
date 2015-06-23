d2b262c66ec1423456f5bbe69d241671f7cb0173
George Richmond <georgerichmond@gmail.com>
2015-06-23 16:02:00 +0100

    Reordering gulp task statements so the end event of the rebundle task fires at the end only.

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,12 @@ function compile(watch) {
     function rebundle() {
         bundler.bundle()
             .on('error', function(err) { console.error(err); this.emit('end'); })
-            .on('end',  function() { process.exit(0); })
             .pipe(source('build.js'))
             .pipe(buffer())
             .pipe(sourcemaps.init({ loadMaps: true }))
             .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest('./build'));
+            .pipe(gulp.dest('./build'))
+            .on('end',  function() { process.exit(0); });
 
     }
 
