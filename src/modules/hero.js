bbad32dfce11ffcc880f3ae1140272bd729631e6
Mathieu Davy <matt@ekynoxe.com>
2015-06-25 15:15:17 +0100

    @ekynoxe - Some small style changes.

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -7,7 +7,6 @@ class Hero extends React.Component {
             <div className="hero__content">
                 <h1>7 / 7 ten years on</h1>
                 <p>On July 7 2005, four suicide bmbers attacked central London, killing 52 people and injuring hundreds more.</p>
-                <button>Scroll down for full story</button>
             </div>
         </section>
     }
