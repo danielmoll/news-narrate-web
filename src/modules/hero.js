ea9816346568e19d77243dc554294dbfe535a76a
Mathieu Davy <matt@ekynoxe.com>
2015-06-29 09:22:13 +0100

    @ekynoxe - Adding hero background video with autoplay and muted.

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -4,9 +4,12 @@ import _ from 'lodash';
 class Hero extends React.Component {
     render () {
         return <section className="hero">
+            <video autoPlay muted loop poster="/build/img/heroBg.jpg" className="hero__video">
+                <source src="http://res.cloudinary.com/skynews/video/upload/v1435331937/narrate/london-bombing-7-7/intro-video.mp4" type="video/mp4"></source>
+            </video>
             <div className="hero__content">
-                <h1>7 / 7 ten years on</h1>
-                <p>On July 7 2005, four suicide bmbers attacked central London, killing 52 people and injuring hundreds more.</p>
+                <h1 className="hero__title">7 / 7 ten years on</h1>
+                <p className="hero__text">On July 7 2005, four suicide bmbers attacked central London, killing 52 people and injuring hundreds more.</p>
             </div>
         </section>
     }
