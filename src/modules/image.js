c27a0b624b4977b42c53e4c46c0319a40ab1c548
George Richmond <georgerichmond@gmail.com>
2015-07-02 15:11:11 +0100

    GR: use smaller size images for faster loading on mobile

--- src/modules/image.js
+++ src/modules/image.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class Image extends React.Component {
+
+    resizedSource(src) {
+        var width = window.innerWidth;
+        if (width > 1000) {
+            return src.replace('upload/', 'upload/q_80/');
+        } else if (width > 800) {
+            return src.replace('upload/', 'upload/c_scale,w_800,q_80/');
+        } else if (width > 600) {
+            return src.replace('upload/', 'upload/c_scale,w_600,q_80/');
+        } else {
+            return src.replace('upload/', 'upload/c_scale,w_500,q_80/');
+        }
+    }
+
     render() {
-    	var classNames = (this.props.classNames ? ' ' + this.props.classNames : '');
+
+        var classNames = (this.props.classNames ? ' ' + this.props.classNames : '');
         return (
-            <div className={classNames}><img className="image" src={this.props.data} /></div>
+            <div className={classNames}><img className="image" src={this.resizedSource(this.props.data)}/></div>
         )
     }
 }
