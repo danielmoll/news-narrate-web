5afa696b9086a77888dc0fc90f2f959ab28f3090
George Richmond <georgerichmond@gmail.com>
2015-07-02 13:55:39 +0100

    Adding auto video codec to hero video

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -5,7 +5,7 @@ class Hero extends React.Component {
     render () {
         return <section className="hero">
             <video autoPlay muted loop poster="/build/img/heroBg.jpg" className="hero__video">
-                <source src="http://res.cloudinary.com/skynews/video/upload/v1435760116/landingpage-bg_e0klcl.mp4" type="video/mp4"></source>
+                <source src="http://res.cloudinary.com/skynews/video/upload/vc_auto/v1435760116/landingpage-bg_e0klcl.mp4" type="video/mp4"></source>
             </video>
             <div className="hero__content">
                 <h1 className="hero__title">7 / 7 ten years on</h1>
