7b6733e99822a4a31a85f1f0eda492f9c1fbe1b7
Mathieu Davy <matt@ekynoxe.com>
2015-07-03 15:15:14 +0100

    @ekynoxe - Fixing order+key of interviews.

--- src/modules/interviews.js
+++ src/modules/interviews.js
@@ -16,6 +16,7 @@ class Interviews extends React.Component {
 
         var propInterviews = (this.props.data && this.props.data.interviews) || [],
             interviews = [],
+            interviewsToSort = {},
             id = this.props.id,
             newDocTitle,
             ogImage;
@@ -24,16 +25,21 @@ class Interviews extends React.Component {
             newDocTitle = 'July 7 bombings - Sky News speaks to ' + propInterviews[id].name;
             ogImage = propInterviews[id].thumbnail;
             interviews.push(<Interview data={ {interview: propInterviews[id], globalData: this.props.data, expanded: true} } key={ 'interview_' + id} />)
-        } else {
 
+        } else {
             _.forEach(propInterviews, function (interview, key) {
                 if (interview) {
                     interview.id = key;
+                    interviewsToSort[key] = interview;
+                }
+            });
+
+            _.forEach(_.sortBy(interviewsToSort, 'order'), function (interview, key) {
+                if (interview) {
                     interviews.push(<Interview data={ {interview: interview, globalData: this.props.data} } key={ 'interview_' + key }/>);
                 }
             }.bind(this));
 
-            propInterviews = _.sortBy(propInterviews, 'order');
         }
 
         const metaData = {
