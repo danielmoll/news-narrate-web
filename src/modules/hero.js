3bbe303ca4837825f1b677a832066b661a788379
Mathieu Davy <matt@ekynoxe.com>
2015-07-06 15:42:32 +0100

    @ekynoxe - Not adding the video tag at all if window width <= 768px

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import _ from 'lodash';
 
 class Hero extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.render = this.render.bind(this);
+        this._handleResize = this._handleResize.bind(this);
+        this.state = {
+            showVideo: false
+        };
+    }
+
+    _handleResize(e) {
+        if (window.innerWidth >= 769) {
+            if (!this.state.showVideo) {
+                this.setState({ showVideo: true});
+            }
+        } else {
+            if (this.state.showVideo) {
+                this.setState({ showVideo: false});
+            }
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this._handleResize);
+        this._handleResize();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this._handleResize);
+    }
+
     render () {
-        return <section className="hero">
-            <video autoPlay muted loop poster="/build/img/heroBg.jpg" className="hero__video">
-                <source src="http://res.cloudinary.com/skynews/video/upload/vc_auto/v1435760116/landingpage-bg_e0klcl.mp4" type="video/mp4"></source>
-            </video>
-            <div className="hero__content">
-                <h1 className="hero__title">7 / 7 ten years on</h1>
-                <p className="hero__text">On 7 July, 2005, four suicide bombers attacked London killing 52 people. Watch the survivors’ stories and see how the day unfolded.</p>
-            </div>
-        </section>
+        var video = '';
+
+        if (this.state.showVideo) {
+            video = (<video autoPlay muted loop poster="/build/img/heroBg.jpg" className="hero__video">
+                        <source src="http://res.cloudinary.com/skynews/video/upload/vc_auto/v1435760116/landingpage-bg_e0klcl.mp4" type="video/mp4"></source>
+                    </video>);
+        }
+
+        return (<section className="hero">
+                { video }
+                <div className="hero__content">
+                    <h1 className="hero__title">7 / 7 ten years on</h1>
+                    <p className="hero__text">On 7 July, 2005, four suicide bombers attacked London killing 52 people. Watch the survivors’ stories and see how the day unfolded.</p>
+                </div>
+            </section>)
     }
 }
 
