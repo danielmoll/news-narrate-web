85ec13a20b61970f781739667f6d4a3f40baa979
Sky News Products Team <skynews.products@gmail.com>
2015-07-20 11:51:01 +0100

    @dm fixed nav at mobile break point - added animation

--- people-smuggler/scripts/navigation.js
+++ people-smuggler/scripts/navigation.js
@@ -3,14 +3,14 @@
 var navigation = document.querySelector('.navigation'),
 	button = document.querySelector('.js-navigation-expand'),
 	navList = document.querySelector('.js-navigation-list'),
-	stop = navigation.getBoundingClientRect().top;
+	stop = navigation.getBoundingClientRect().top + (document.body || document.documentElement).scrollTop;
 
 function handleClick() {
 	navigation.classList.toggle('navigation--active');
 }
 
 function sticky() {
-	var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+	var scrollTop = (document.body || document.documentElement).scrollTop;
 
 	if (scrollTop >= stop) {
 		navigation.classList.add('navigation--sticky');
