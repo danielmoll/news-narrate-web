69f3793152bb40e52eedc686aa483a618adb76b7
Sky News Products Team <skynews.products@gmail.com>
2015-07-20 12:27:46 +0100

    @dm nav sticky check fires on resize

--- people-smuggler/scripts/navigation.js
+++ people-smuggler/scripts/navigation.js
@@ -3,16 +3,22 @@
 var navigation = document.querySelector('.navigation'),
 	button = document.querySelector('.js-navigation-expand'),
 	navList = document.querySelector('.js-navigation-list'),
+	stop;
+
+function setStop() {
 	stop = navigation.getBoundingClientRect().top + (document.body || document.documentElement).scrollTop;
+}
 
 function handleClick() {
 	navigation.classList.toggle('navigation--active');
 }
 
-function sticky() {
-	var scrollTop = (document.body || document.documentElement).scrollTop;
+function isSticky() {
+	return ((document.body || document.documentElement).scrollTop >= stop);
+}
 
-	if (scrollTop >= stop) {
+function handleNavEvent() {
+	if (isSticky()) {
 		navigation.classList.add('navigation--sticky');
 	} else {
 		navigation.classList.remove('navigation--sticky');
@@ -20,4 +26,8 @@ function sticky() {
 }
 
 button.addEventListener('click', handleClick);
-window.addEventListener('scroll', sticky);
+window.addEventListener('scroll', handleNavEvent);
+window.addEventListener('resize', setStop);
+
+setStop();
+handleNavEvent();
