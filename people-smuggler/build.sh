88b836d9f0b45fdcd2910f2d554b7531c0e303af
Sky News Products Team <skynews.products@gmail.com>
2015-07-21 15:54:12 +0100

    @ekynoxe - Fixing purge step and calling people-smuggler build script

--- people-smuggler/build.sh
+++ people-smuggler/build.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
+echo '-----------------------------------'
+echo 'Browserifying people-smuggler'
 browserify scripts/main.js --debug | uglifyjs > scripts/bundle.js
+echo 'Browserify complete'
+echo '-----------------------------------'
