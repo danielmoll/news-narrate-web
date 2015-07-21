cadae1602b4ed15e6939fb68cd45065f438df8d5
Sky News Products Team <skynews.products@gmail.com>
2015-07-21 11:02:44 +0100

    @dm cross browser bug fixing nav

--- people-smuggler/scripts/navigation.js
+++ people-smuggler/scripts/navigation.js
@@ -4,7 +4,11 @@ var navigation = document.querySelector('.navigation'),
 	navigationPosition = navigation.getBoundingClientRect(),
 	button = document.querySelector('.js-navigation-expand'),
 	navList = document.querySelector('.js-navigation-list'),
+	placeholder = document.createElement('div'),
 	isAdded = false;
+
+placeholder.style.width = navigationPosition.width + 'px';
+placeholder.style.height = navigationPosition.height + 'px';
 
 function handleClick() {
 	navigation.classList.toggle('navigation--active');
@@ -13,9 +17,11 @@ function handleClick() {
 function handleNavEvent() {
 	if (window.pageYOffset >= navigationPosition.top && !isAdded) {
 		navigation.classList.add('navigation--sticky');
+		navigation.parentNode.insertBefore(placeholder, navigation);
 		isAdded = true;
 	} else if (window.pageYOffset < navigationPosition.top && isAdded) {
 		navigation.classList.remove('navigation--sticky');
+		navigation.parentNode.removeChild(placeholder);
 		isAdded = false;
 	}
 }
