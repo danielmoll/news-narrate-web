8e94759eacafb0241a58906d00bff90bcee3d861
Sky News Products Team <skynews.products@gmail.com>
2015-07-22 11:45:58 +0100

    @jw @dm updated hero to 16x9 and testing video hero play in touch

--- people-smuggler/scripts/documentary.js
+++ people-smuggler/scripts/documentary.js
@@ -1,14 +1,20 @@
 var playButton = document.querySelector('.js-play-button'),
+	closeButton = document.querySelector('.js-button-close'),
 	documentary = document.querySelector('.js-documentary'),
-	video = document.querySelector('.js-documentary-video');
+	video = document.querySelector('.js-documentary-video'),
+	ACTIVE_CLASS = 'documentary--active';
 
-playButton.onclick = toggleVideo;
+playButton.onclick = playVideo;
+closeButton.onclick = pauseVideo;
 
-function toggleVideo(e) {
-	documentary.classList.toggle('documentary--active');
-	if (video.paused) {
-		video.play();
-	} else {
-		video.pause();
-	}
+if (window.detect.touch) closeButton.style.display = 'none';
+
+function playVideo(e) {
+	if (!window.detect.touch) documentary.classList.add(ACTIVE_CLASS);
+	video.play();
+}
+
+function pauseVideo(e) {
+	if (!window.detect.touch) documentary.classList.remove(ACTIVE_CLASS);
+	video.pause();
 }
