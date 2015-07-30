15acc909004e9f2087708920d6e6070fd5ff8701
Dan Forys <d@forys.co.uk>
2015-07-30 16:02:08 +0100

    WIP narrate templating with swig

--- people-smuggler/build.sh
+++ people-smuggler/build.sh
@@ -4,3 +4,7 @@ echo 'Browserifying people-smuggler'
 browserify scripts/main.js --debug | uglifyjs > scripts/bundle.js
 echo 'Browserify complete'
 echo '-----------------------------------'
+echo 'Rendering templates'
+node_modules/swig/bin/swig.js render templates/main.swig > index.html
+echo 'Template render complete'
+echo '-----------------------------------'
