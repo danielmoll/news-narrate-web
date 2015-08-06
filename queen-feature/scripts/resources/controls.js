a2d8e827ab62a6035cfff99e9cd938a575c355da
Sky News Products Team <skynews.products@gmail.com>
2015-08-06 16:38:32 +0100

    @joe-strummer, @ekynoxe - Removing plugin fully in favour of 4 quarters to control jump and direction. TODO: We’ll need “tutorial” icons overlays to teach users how to navigate.

--- queen-feature/scripts/resources/controls.js
+++ queen-feature/scripts/resources/controls.js
@@ -7,11 +7,6 @@ Game.Controls = function(game, player) {
 
 Game.Controls.prototype.create = function() {
     this.cursors = this.game.input.keyboard.createCursorKeys();
-    // this.game.touchControl = this.game.plugins.add(Phaser.Plugin.TouchControl);
-    // this.game.touchControl.inputEnable();
-    
-    // this.jumpButton = this.game.add.button(this.game.camera.width -120,  this.game.camera.height - 120, 'touch', this.jump, this);
-    // this.jumpButton.fixedToCamera = true;
 };
 
 Game.Controls.prototype.moveLeft = function(speed) {
@@ -46,51 +41,24 @@ Game.Controls.prototype.update = function () {
         if (this.game.input.activePointer.x < this.game.width / 2) {
             this.moveLeft();
 
+            if (this.game.input.activePointer.y < this.game.height / 2) {
+                this.jump();
+                this.jumped = true;
+            }
+
         } else {
             this.moveRight();
-        }
 
-        if (!this.jumped) {
-            this.jump();
-            this.jumped = true;
+            if (this.game.input.activePointer.y < this.game.height / 2) {
+                this.jump();
+                this.jumped = true;
+            }
         }
 
     } else if (this.game.input.activePointer.isUp) {
         this.player.body.velocity.x = 0;
         this.jumped = false;
     }
-
-    // if (this.player.body.onFloor()) {
-    //     this.jumped = false;
-    // }
-
-
-    // if (this.player.body.onFloor()) {
-    //     this.jumped = false;
-    // }
-
-    // if (this.cursors.up.isDown) {
-    //     if (this.player.body.onFloor()) {
-    //         this.jump();
-    //     }
-    // }
-
-    // if (this.cursors.left.isDown) {
-    //     this.moveLeft();
-
-    // } else if (this.cursors.right.isDown) {
-    //     this.moveRight();
-        
-    // } else if (this.game.touchControl.cursors.left) {
-    //     this.moveLeft(this.game.touchControl.speed.x);
-
-    // } else if (this.game.touchControl.cursors.right) {
-    //     this.moveRight(this.game.touchControl.speed.x);
-
-    // } else {
-    //     this.player.body.velocity.x = 0;
-    //     this.jumped = false;
-    // }
 
     if (this.player.body.velocity.x === 0) {
         this.player.animations.stop();
