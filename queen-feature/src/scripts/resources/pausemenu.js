2d8472d0d4ede0d846fd094adc5ff42c56d1f62c
Sky News Products Team <skynews.products@gmail.com>
2015-08-18 10:55:19 +0100

    @joe-strummer, @ekynoxe - Fixing that damn f***** next level transition from the pause menu. The fadeIn plugin has a problem with scope. Not using it right now. Screw this.

--- queen-feature/src/scripts/resources/pausemenu.js
+++ queen-feature/src/scripts/resources/pausemenu.js
@@ -1,8 +1,11 @@
-/* global Phaser, Game */
+/* global Game */
 'use strict';
 
 Game.PauseMenu = function(game) {
     this.game = game;
+    this.buttons = [];
+    this.resumeButton = null;
+    this.nextLevelButton = null;
 
     this.buttonDefinitions = [
         {
@@ -15,7 +18,13 @@ Game.PauseMenu = function(game) {
             sprite: 'resume_button',
             x: 40,
             y: 120,
-            callback: 'resume'
+            callback: 'resume',
+        },
+        {
+            sprite: 'next_level_button',
+            x: 40,
+            y: 120,
+            callback: 'nextLevel'
         },
         {
             sprite: 'restart_level_button',
@@ -37,27 +46,32 @@ Game.PauseMenu = function(game) {
         }
     ];
 
-    this.buttons = [];
+    this.pauseButtonDefinitions = [
+    ];
 
     this.addPauseButton();
-    this.drawMenu();
 
+    this.drawMenu();
     this.game.input.onDown.add(this.handleInput, this);
 };
 
 Game.PauseMenu.prototype.addPauseButton = function() {
-    this.pauseButton = this.game.add.button(10, 10, 'pause', this.showPauseMenu, this);
+    this.pauseButton = this.game.add.button(10, 10, 'pause', function(){ this.showMenu(true); }.bind(this), this);
     this.pauseButton.fixedToCamera = true;
 };
 
 Game.PauseMenu.prototype.drawMenu = function() {
-    var pauseText,
+    var // pauseText,
         btn,
         bg;
 
+    // Resetting buttons locations.
+    this.buttons = [];
+
+    // Drawing the base menu elements
     this.menuGroup = this.game.add.group();
     this.menuGroup.visible = false;
- 
+
     bg = this.game.add.graphics(0, 0);
     bg.fixedToCamera = true;
     bg.beginFill(0X000, 0.8);
@@ -66,12 +80,14 @@ Game.PauseMenu.prototype.drawMenu = function() {
 
     this.menuGroup.add(bg);
 
-    pauseText = new Phaser.Text(this.game, 240, 40, "Paused", {font: 'helvetica', fontSize: 18, fill: '#ffffff'});
-    pauseText.fixedToCamera = true;
-    this.menuGroup.add(pauseText);
+    // if (pause) {
+    //     pauseText = new Phaser.Text(this.game, 240, 40, "Paused", {font: 'helvetica', fontSize: 18, fill: '#ffffff'});
+    //     pauseText.fixedToCamera = true;
+    //     this.menuGroup.add(pauseText);
+    // }
 
     this.buttonDefinitions.forEach(function(button) {
-        btn = new Phaser.Image(this.game, button.x, button.y, button.sprite);
+        btn = this.game.add.image(button.x, button.y, button.sprite);
         btn.fixedToCamera = true;
         this.menuGroup.add(btn);
 
@@ -80,8 +96,17 @@ Game.PauseMenu.prototype.drawMenu = function() {
             y1: btn.y,
             x2: btn.x + btn.width,
             y2: btn.y + btn.height,
-            callback: button.callback
+            callback: button.callback,
+            sprite: button.sprite
         });
+        
+        if (button.sprite === 'resume_button') {
+            this.resumeButton = btn;
+        }
+
+        if (button.sprite === 'next_level_button') {
+            this.nextLevelButton = btn;
+        }
 
     }.bind(this));
 
@@ -92,11 +117,26 @@ Game.PauseMenu.prototype.handleInput = function(event) {
         return;
     }
 
+    var callback;
+
     this.buttons.forEach(function(button) {
-        if(event.x > button.x1 && event.x < button.x2 && event.y > button.y1 && event.y < button.y2 ){
-            button.callback && this[button.callback]();
+        if (!callback) {
+            if(event.x > button.x1 && event.x < button.x2 && event.y > button.y1 && event.y < button.y2 ) {
+                
+                if (button.sprite === 'resume_button' && this.resumeButton.visible === true) {
+                    callback = button.callback;
+
+                } else if (button.sprite === 'next_level_button' && this.nextLevelButton.visible === true) {
+                    callback = button.callback;
+
+                } else if (button.sprite !== 'resume_button' && button.sprite !== 'next_level_button') {
+                    callback = button.callback;
+                }
+            }
         }
     }.bind(this));
+
+    callback && this[callback]();
 };
 
 Game.PauseMenu.prototype.backToNavigation = function() {
@@ -118,8 +158,18 @@ Game.PauseMenu.prototype.restartLevel = function() {
     this.game.state.start(this.game.state.current);
 };
 
-Game.PauseMenu.prototype.showPauseMenu = function() {
+Game.PauseMenu.prototype.showMenu = function(state) {
     this.game.paused = true;
+    
+    if (state === 'next_level') {
+        // this.nextLevelCallback = nextLevelCallback;
+        this.resumeButton.visible = false;
+        this.nextLevelButton.visible = true;
+    } else {
+        this.resumeButton.visible = true;
+        this.nextLevelButton.visible = false;
+    }
+
     this.pauseButton.visible = false;
     this.menuGroup.visible = true;
 };
@@ -130,4 +180,17 @@ Game.PauseMenu.prototype.shareOnFacebook = function() {
 
 Game.PauseMenu.prototype.shareOnTwitter = function() {
     console.log('shared on Twitter');
+};
+
+Game.PauseMenu.prototype.nextLevel = function () {
+    this.game.paused = false;
+
+    var nextLevel = this.game.state.states[this.game.state.current].nextLevelKey;
+
+    if(Game.Score.allCollected()) {
+        nextLevel = 'end_screen';
+    }
+
+    this.game.analytics.stateComplete(this.game.state.current);
+    this.game.state.start(nextLevel);
 };
