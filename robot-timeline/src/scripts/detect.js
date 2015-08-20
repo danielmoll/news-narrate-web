35ad85399703c79327c3f9b334f269a688403d5d
Sky News Products Team <skynews.products@gmail.com>
2015-08-20 10:42:29 +0100

    Initial commit for robot timeline

---
+++ robot-timeline/src/scripts/detect.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var html = document.querySelector('html'),
+	detectedFeatures = [],
+	detectedFeaturesMap = {},
+	tests = {},
+	result;
+
+tests.touch = function() {
+	return (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+};
+
+for (var test in tests) {
+	result = tests[test]();
+	detectedFeatures.push(result ? test : 'no-' + test);
+	detectedFeaturesMap[result ? test : 'no-' + test] = true;
+}
+
+detectedFeatures.forEach(function(feature) {
+	html.classList.add(feature);
+});
+
+module.exports = detectedFeatures;
+window.detect = detectedFeaturesMap;
