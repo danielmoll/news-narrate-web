f1b27905af4ffd3182d047dc816eb735f80d2566
Sky News Products Team <skynews.products@gmail.com>
2015-08-27 09:47:09 +0100

    @ekynoxe - Updating Alex’s house tutorial level.

--- queen-feature/src/scripts/state/intro_alexs_house.js
+++ queen-feature/src/scripts/state/intro_alexs_house.js
@@ -1,10 +1,49 @@
-/* global Phaser, Game */
+/* global Game */
 'use strict';
 
 Game.State.Intro_AlexsHouse = function() {
-    this.forwardText = null;
-    this.backwardText = null;
-    this.jumpText = null;
+
+    // this.textElts = [{
+    //     id: 'forwardText',
+    //     text: 'Press here to\nmove forward',
+    //     size: 16,
+    //     color: '#000',
+    //     position: 'br', // bottom right
+    //     alpha: function(x) {
+    //         var alpha = 0,
+    //             t;
+
+    //         t = 1 - (x - this.game.width / 2) / (this.game.width / 2);
+    //         alpha = t < 0 ? 0 : (t > 1 ? 1 : t);
+
+    //         return alpha;
+    //     }.bind(this)
+    // },{
+    //     id: 'jumpText',
+    //     text: 'While moving,\npress here to jump',
+    //     size: 16,
+    //     color: '#000',
+    //     position: 'bl', // bottom left
+    //     alpha: function(x) {
+    //         var alpha = 0,
+    //             t;
+
+    //         if (x >= this.game.width / 2 && x < this.game.width) {
+    //             console.log('aaaaaa', x);
+    //             t = (x - this.game.width / 2) / (this.game.width / 2);
+    //             console.log(t);
+    //             alpha = t < 0 ? 0 : (t > 1 ? 1 : t);
+
+    //         } else if (x >= this.game.width && x < this.game.width * 1.5) {
+    //             // console.log('bbbbb');
+    //             t = 1 - (x - this.game.width) / (this.game.width / 2);
+    //             // console.log(t);
+    //             alpha = t;
+    //         }
+
+    //         return alpha;
+    //     }.bind(this)
+    // }];
 };
 Game.State.Intro_AlexsHouse.prototype = new Game.State.BaseState();
 Game.State.Intro_AlexsHouse.prototype.levelKey = 'alexs_house';
@@ -21,25 +60,33 @@ Game.State.Intro_AlexsHouse.prototype.createBackgroundLayers = function() {
 };
 
 Game.State.Intro_AlexsHouse.prototype.createForegroundLayers = function() {
-    var gameWidth = this.game.width,
-        gameHeight = this.game.height,
-        color = '#000',
-        size = 16;
+    // var gameWidth = this.game.width,
+    //     gameHeight = this.game.height;
 
     this.levelModule.createLayer('foreground');
 
-    this.forwardText = this.game.add.text(0, 0, 'Press here to\nmove forward', { font: size + 'px silkscreennormal', align: 'center', fill: color} );
-    this.forwardText.fixedToCamera = true;
+    // this.textElts.forEach(function (elt) {
+    //     var txtElt = this.game.add.text(0, 0, elt.text, { font: elt.size + 'px silkscreennormal', align: 'center', fill: elt.color} );
+    //     txtElt.fixedToCamera = true;
 
-    this.forwardText.cameraOffset.x = gameWidth - this.forwardText.width - 20;
-    this.forwardText.cameraOffset.y = gameHeight - this.forwardText.height - 10;
+    //     if (elt.position === 'br') {
+    //         txtElt.cameraOffset.x = gameWidth - txtElt.width - 20;
+    //         txtElt.cameraOffset.y = gameHeight - txtElt.height - 10;
+
+    //     } else if (elt.position === 'bl') {
+    //         txtElt.cameraOffset.x = 20;
+    //         txtElt.cameraOffset.y = gameHeight - txtElt.height - 10;
+    //     }
+
+    //     this[elt.id] = txtElt;
+    //     this[elt.id].getAlpha = elt.alpha;
+    // }.bind(this));
 };
 
 Game.State.Intro_AlexsHouse.prototype.updateState = function() {
-    var diff = this.game.width / 2 - Math.abs(this.world.x),
-        alpha;
+    // var viewCentre = this.game.width / 2 - this.world.x;
 
-    diff = diff > 0  ? diff : 0;
-
-    this.forwardText.alpha = diff / (this.game.width / 2);
+    // this.textElts.forEach(function (elt) {
+    //     this[elt.id].alpha = elt.alpha(viewCentre);
+    // }.bind(this));
 };
