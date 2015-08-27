c38fbf01d5d81944cb33acb1422a42269c8b3288
Sky News Products Team <skynews.products@gmail.com>
2015-08-27 10:37:03 +0100

    @ekynoxe - Extracting label button into its own file, and adding a skip button to the intro screen.

--- queen-feature/src/scripts/state/intro_alexs_house.js
+++ queen-feature/src/scripts/state/intro_alexs_house.js
@@ -1,50 +1,7 @@
-/* global Game */
+/* global Game, LabelButton */
 'use strict';
 
-Game.State.Intro_AlexsHouse = function() {
-
-    // this.textElts = [{
-    //     id: 'forwardText',
-    //     text: 'Press here to\nmove forward',
-    //     size: 16,
-    //     color: '#000',
-    //     position: 'br', // bottom right
-    //     alpha: function(x) {
-    //         var alpha = 0,
-    //             t;
-
-    //         t = 1 - (x - this.game.width / 2) / (this.game.width / 2);
-    //         alpha = t < 0 ? 0 : (t > 1 ? 1 : t);
-
-    //         return alpha;
-    //     }.bind(this)
-    // },{
-    //     id: 'jumpText',
-    //     text: 'While moving,\npress here to jump',
-    //     size: 16,
-    //     color: '#000',
-    //     position: 'bl', // bottom left
-    //     alpha: function(x) {
-    //         var alpha = 0,
-    //             t;
-
-    //         if (x >= this.game.width / 2 && x < this.game.width) {
-    //             console.log('aaaaaa', x);
-    //             t = (x - this.game.width / 2) / (this.game.width / 2);
-    //             console.log(t);
-    //             alpha = t < 0 ? 0 : (t > 1 ? 1 : t);
-
-    //         } else if (x >= this.game.width && x < this.game.width * 1.5) {
-    //             // console.log('bbbbb');
-    //             t = 1 - (x - this.game.width) / (this.game.width / 2);
-    //             // console.log(t);
-    //             alpha = t;
-    //         }
-
-    //         return alpha;
-    //     }.bind(this)
-    // }];
-};
+Game.State.Intro_AlexsHouse = function() {};
 Game.State.Intro_AlexsHouse.prototype = new Game.State.BaseState();
 Game.State.Intro_AlexsHouse.prototype.levelKey = 'alexs_house';
 Game.State.Intro_AlexsHouse.prototype.nextLevelKey = 'navigation';
@@ -60,33 +17,11 @@ Game.State.Intro_AlexsHouse.prototype.createBackgroundLayers = function() {
 };
 
 Game.State.Intro_AlexsHouse.prototype.createForegroundLayers = function() {
-    // var gameWidth = this.game.width,
-    //     gameHeight = this.game.height;
-
     this.levelModule.createLayer('foreground');
 
-    // this.textElts.forEach(function (elt) {
-    //     var txtElt = this.game.add.text(0, 0, elt.text, { font: elt.size + 'px silkscreennormal', align: 'center', fill: elt.color} );
-    //     txtElt.fixedToCamera = true;
-
-    //     if (elt.position === 'br') {
-    //         txtElt.cameraOffset.x = gameWidth - txtElt.width - 20;
-    //         txtElt.cameraOffset.y = gameHeight - txtElt.height - 10;
-
-    //     } else if (elt.position === 'bl') {
-    //         txtElt.cameraOffset.x = 20;
-    //         txtElt.cameraOffset.y = gameHeight - txtElt.height - 10;
-    //     }
-
-    //     this[elt.id] = txtElt;
-    //     this[elt.id].getAlpha = elt.alpha;
-    // }.bind(this));
+    new LabelButton(this.game, 80, 420, null, '[SKIP]', this.skip, this, { font: '25px silkscreennormal', fill: '#f00' });
 };
 
-Game.State.Intro_AlexsHouse.prototype.updateState = function() {
-    // var viewCentre = this.game.width / 2 - this.world.x;
-
-    // this.textElts.forEach(function (elt) {
-    //     this[elt.id].alpha = elt.alpha(viewCentre);
-    // }.bind(this));
+Game.State.Intro_AlexsHouse.prototype.skip = function() {
+    this.game.state.start('navigation');
 };
--- queen-feature/src/scripts/state/navigation.js
+++ queen-feature/src/scripts/state/navigation.js
@@ -1,36 +1,5 @@
-/* global Phaser, Game */
+/* global Phaser, Game, LabelButton */
 'use strict';
-
-var LabelButton = function(game, x, y, key, label, callback, callbackContext, labelOptions, overFrame, outFrame, downFrame, upFrame) {
-    var options = { font: '35px silkscreennormal', fill: '#fff'},
-        k;
-
-    for (k in labelOptions) {
-        options[k] = labelOptions[k];
-    }
-
-    Phaser.Button.call(this, game, x, y, key, callback, callbackContext, overFrame, outFrame, downFrame, upFrame);
-
-    this.anchor.setTo( 0.5, 0.5 );
-    this.label = new Phaser.Text(game, 0, 0, label, options );
-
-    // Puts the label in the center of the button
-    // Not 0.5, 0.5 because of the font height offset
-    this.label.anchor.setTo( 0.5, 0.63 );
-
-    this.addChild(this.label);
-    this.setLabel( label );
-
-    //adds button to game
-    game.add.existing( this );
-};
-
-LabelButton.prototype = Object.create(Phaser.Button.prototype);
-LabelButton.prototype.constructor = LabelButton;
-
-LabelButton.prototype.setLabel = function( label ) {
-   this.label.setText(label);
-};
 
 Game.State.Navigation = function() {};
 Game.State.Navigation.prototype = {
