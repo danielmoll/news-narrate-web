a36cf2f78e0b672e2b0d482fb38a76cd1f92fb64
Sky News Products <skynews.products@gmail.com>
2015-08-28 16:07:11 +0100

    @dm @gee-oge added correct images and latest copy

--- robot-revolution/src/scripts/main.js
+++ robot-revolution/src/scripts/main.js
@@ -5,14 +5,10 @@ require('./detect');
 var $ = require('jquery'),
 	inView = require('./in-view'),
 	scene = require('./scene'),
-	images = require('./images'),
-	resize = require('./resize'),
 	share = require('./share');
 
 $(function() {
 	scene.init();
-	resize.init();
-	images.init();
 	share.init();
 
 	inView.init(); // this has to fire last
