9f342bc8e12c433a8517ea1033eb365dec522f21
Sky News Products <skynews.products@gmail.com>
2015-09-01 13:47:11 +0100

    @ekynoxe @gee-oge removed scene.js

--- robot-revolution/src/scripts/main.js
+++ robot-revolution/src/scripts/main.js
@@ -4,11 +4,11 @@ require('./detect');
 
 var $ = require('jquery'),
 	inView = require('./in-view'),
-	scene = require('./scene'),
+	// scene = require('./scene'),
 	share = require('./share');
 
 $(function() {
-	scene.init();
+	// scene.init();
 	share.init();
 
 	inView.init(); // this has to fire last
