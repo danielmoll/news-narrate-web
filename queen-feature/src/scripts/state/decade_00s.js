f7739caa7e8f85f88b5f4784f5dca50c5b752788
Peji Joe Faghihi <peji.faghihi@sky.uk>
2015-09-10 10:46:29 +0100

    Fixed 2000's next level key

--- queen-feature/src/scripts/state/decade_00s.js
+++ queen-feature/src/scripts/state/decade_00s.js
@@ -4,7 +4,7 @@
 Game.State.Decade_00s = function() {};
 Game.State.Decade_00s.prototype = new Game.State.BaseState();
 Game.State.Decade_00s.prototype.levelKey = 'decade_00s';
-Game.State.Decade_00s.prototype.nextLevelKey = 'decade_2010s';
+Game.State.Decade_00s.prototype.nextLevelKey = 'decade_now';
 
 Game.State.Decade_00s.prototype.setCollisions = function() {
     this.levelModule.tilemap.setCollision(34);
