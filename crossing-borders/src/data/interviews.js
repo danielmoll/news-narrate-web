d3068bdcd703de7ff331e0043f9ee4176acb710c
danforys <d@forys.co.uk>
2015-09-18 16:29:28 +0100

    Interview quotes updates

--- crossing-borders/src/data/interviews.js
+++ crossing-borders/src/data/interviews.js
@@ -19,7 +19,7 @@ Format:
 var interviews = {
     "andrewwilson": {
         "location": "Destination Europe",
-        "name": "Andrew Wilson",
+        "name": "Sky's Andrew Wilson talks to Syrian migrants",
         "quote": "We don’t know where we stay tonight",
         "text": "Sky’s Andrew Wilson talks to groups of Syrian migrants attempting to cross Hungary’s border in the middle of the night, many in the hope of reuniting with family and friends in Holland.",
         "thumbnail": "http://res.cloudinary.com/skynews/image/upload/v1442580129/migration-crisis/thumbnails/andrew-wilson.jpg",
@@ -28,7 +28,7 @@ var interviews = {
         }
     },
     "mustafa": {
-        "location": "Syria",
+        "location": "Refugee from Syria",
         "name": "Mustafa",
         "quote": "My home was destroyed",
         "text": "Sky’s Harriet Hadfield talks to Syrian boy Mustafa. He tells her he has no friends in the migrant camp, and wants to reach Germany to continue his schooling.",
@@ -38,7 +38,7 @@ var interviews = {
         }
     },
     "fadi": {
-        "location": "Damascus, Syria",
+        "location": "Refugee from Damascus",
         "name": "Fadi",
         "quote": "Looking for anywhere I can find a home",
         "text": "Fadi used to work in Arts in Damascus before civil war forced him to flee. He explains the living conditions he and thousands others endure in their journey for a new life.",
@@ -48,7 +48,7 @@ var interviews = {
         }
     },
     "ahmed": {
-        "location": "Aleppo, Syria",
+        "location": "Refugee from Aleppo, Syria",
         "name": "Ahmed",
         "quote": "We slept in the street for ten days",
         "text": "Sky’s Harriet Hadfield talks to Ahmed who wants to get to Germany to finish his education and train to be a doctor.",
@@ -58,7 +58,7 @@ var interviews = {
         }
     },
     "raafat": {
-        "location": "Damascus, Syria",
+        "location": "Refugee from Damascus",
         "name": "Raafat",
         "quote": "We want to return to our homes… but not now",
         "text": "Rafaat says after waiting five years for war to end, he left Syria to try and build a new future for his children.",
@@ -68,9 +68,9 @@ var interviews = {
         }
     },
     "harriet": {
-        "location": "My journey through Europe",
+        "location": "Reporting from Serbia",
         "name": "Sky's Harriet Hadfield",
-        "quote": "Hungry, hungry",
+        "quote": "Walking with a stick in the pouring rain",
         "text": "Sky's Harriet Hadfield travels from Serbia to Germany and speaks to refugees trying to find \"freedom\" and a better life.",
         "thumbnail": "http://res.cloudinary.com/skynews/image/upload/v1442580156/migration-crisis/thumbnails/sky-harriet-hadfield.jpg",
         "videos": {
@@ -78,8 +78,8 @@ var interviews = {
         }
     },
     "alexrossi": {
-        "location": "Syria",
-        "name": "Sky’s Alex Rossi and migrant mother",
+        "location": "Reporting from Serbia-Hungary border",
+        "name": "Sky's Alex Rossi talks to a migrant mother",
         "quote": "Please take my daughter to safety",
         "text": "Sky’s Alex Rossi speaks to a Syrian English teacher who says she just wants to pass through Hungary to a country for refuge.",
         "thumbnail": "http://res.cloudinary.com/skynews/image/upload/v1442580151/migration-crisis/thumbnails/sky-alex-rossi.jpg",
@@ -88,7 +88,7 @@ var interviews = {
         }
     },
     "alexcrawford": {
-        "location": "Journey on a dinghy",
+        "location": "Dinghy crossing between Turkey and Greece",
         "name": "Q&A with Sky’s Alex Crawford",
         "quote": "Stop! Stop! Turn around now.",
         "text": "Sky's Alex Crawford joins refugees packed into an inflatable boat as they make the perilous journey across the sea to Greece.",
@@ -99,7 +99,7 @@ var interviews = {
     },
     "alexcrawfordpeoplesmuggler": {
         "location": "Turkey",
-        "name": "People Smuggler",
+        "name": "Sky's Alex Crawford talks to a people smuggler",
         "quote": "I give refugees a new life",
         "text": "Sky’s Alex Crawford speaks to a people smuggler in Izmir, who claims he is providing a valuable service to refugees.",
         "thumbnail": "http://res.cloudinary.com/skynews/image/upload/v1442580141/migration-crisis/thumbnails/people-smuggler.jpg",
