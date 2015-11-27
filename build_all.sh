09a61fad13a6b4e92cba9f5493da0ab2e0e46010
ashleyjkell <ashley.kell@sky.uk>
2015-11-27 12:43:16 +0000

    SSDMDEVOPS-711 Ensure Gulp is run from the main directory

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 npm install
-./node_modules/gulp/bin/gulp.js build
+$WORKSPACE/node_modules/gulp/bin/gulp.js build
 pushd $WORKSPACE/people-smuggler/ && . ./build.sh && popd
 pushd $WORKSPACE/queen-timeline/ && mkdir -p scripts && mkdir -p css && npm i && npm run dist && popd
 pushd $WORKSPACE/queen-feature/ && mkdir -p public/scripts && npm i && npm run dist && popd
