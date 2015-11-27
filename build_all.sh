80aae8e21cb7c6adc2fd126b254bda6844e4a93e
ashleyjkell <ashley.kell@sky.uk>
2015-11-27 12:29:15 +0000

    SSDMDEVOPS-711 Update build_all script

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 npm install
 ./node_modules/gulp/bin/gulp.js build
-pushd people-smuggler/ && . ./build.sh && popd
-pushd queen-timeline/ && mkdir scripts && mkdir css && npm i && npm run dist && popd
-pushd queen-feature/ && mkdir public/scripts && npm i && npm run dist && popd
-pushd robot-revolution/ && mkdir scripts && mkdir css && npm i && npm run dist && popd
-pushd migration-crisis/ && npm i && gulp build && popd
+pushd $WORKSPACE/people-smuggler/ && . ./build.sh && popd
+pushd $WORKSPACE/queen-timeline/ && mkdir -p scripts && mkdir -p css && npm i && npm run dist && popd
+pushd $WORKSPACE/queen-feature/ && mkdir -p public/scripts && npm i && npm run dist && popd
+pushd $WORKSPACE/robot-revolution/ && mkdir -p scripts && mkdir -p css && npm i && npm run dist && popd
+pushd $WORKSPACE/migration-crisis/ && npm i && gulp build && popd
+cd $WORKSPACE
